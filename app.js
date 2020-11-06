7eb81557bbe727aedb706e9c0fece3f2cc9bb0ca
matias <you@example.com>
2020-11-06 12:06:37 -0300

    modificacion

---
+++ app.js
@@ -0,0 +1,6 @@
+//git init para crear un repositorio remoto
+//git status
+
+//crear archivos dentro de la carpeta donde se instalo el repositorio
+//git add para agregar todo en la caja
+//git commit -m"mensage" para etiquetar y mandar ka caha
