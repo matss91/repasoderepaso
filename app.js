daba5a0455898d5a8e86f176219851c72683ec3f
matias <you@example.com>
2020-11-08 20:38:49 -0300

    commit

--- app.js
+++
@@ -1,6 +0,0 @@
-//git init para crear un repositorio remoto
-//git status
-
-//crear archivos dentro de la carpeta donde se instalo el repositorio
-//git add para agregar todo en la caja
-//git commit -m"mensage" para etiquetar y mandar ka caha
